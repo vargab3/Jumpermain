ece1353994a742585ca7c0fe83ed6250bb265e43
vargab3 <vargab3@kkszki.hu>
2024-12-23 11:42:14 +0100

    3 életpont

---
+++ Assets/Scripts/Health/Health.cs
@@ -0,0 +1,29 @@
+
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float startingHealth;
+    public float currentHealth { get; private set; }
+
+    private void Awake()
+    {
+        currentHealth = startingHealth;
+    }
+
+    public void TakeDamage(float _damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+
+        if (currentHealth > 0)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+
+    
+}
---
+++ Assets/Scripts/Health/Healthbar.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Healthbar : MonoBehaviour
+{
+    [SerializeField] private Health playerHealth;
+    [SerializeField] private Image totalehealthBar;
+    [SerializeField] private Image currenthealthBar;
+
+    private void Start()
+    {
+        totalehealthBar.fillAmount = playerHealth.currentHealth / 10; 
+    }
+
+    private void Update()
+    {
+        currenthealthBar.fillAmount = playerHealth.currentHealth / 10;
+    }
+}
