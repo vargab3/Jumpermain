b1ecb81d4949433680d66f200329777ff5e7a02a
tothcsaba2006 <tothcs@kkszki.hu>
2025-01-09 10:09:50 +0100

    vege

---
+++ Assets/FinishPoint.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishPoint : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadSceneAsync("main menu");
+    }
+}
